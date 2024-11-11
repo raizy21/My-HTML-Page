67980e18f4b77ada9d13ccfd0b1ddf181b38ede2
Razvan Andrei <chiper_raizy@yahoo.com>
2024-11-11 06:20:49 +0100

    first project

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  alert(
+    "These projects can help me build my skills in HTML, CSS and JavaScript. Remember, the key is to start small and gradually take on more complex projects as you become more comfortable with coding. Enjoy!"
+  );
+}
+
+function myFunction2() {
+  alert("Welcome to My Homepage!!!");
+}
+function myFunction3() {
+  alert("I am Freeman. I am Web Developer. I am Traveller.");
+}
+function myFunction4() {
+  alert("andreirazy@gmail.com <= Please contact me anytime.");
+}
